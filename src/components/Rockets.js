25f13b43746d3294dce8cf8581e4c5a44e999798
Sowmya <srisow1359@gmail.com>
2022-08-09 00:02:49 +0530

    optimised

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,38 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { setSearchValue } from '../redux/reducer'
-import { useDispatch } from "react-redux";
-import {Link} from "react-router-dom"
+import List from "./List"
 export default function Rockets(){
     const [rockets, setRockets]=useState([]);
-    const dispatch = useDispatch();
     useEffect(()=>{
         if(rockets.length<=0)
         fetch("https://api.spacexdata.com/v3/rockets")
         .then(res => res.json())
-        .then(y => {console.log(y); setRockets(y)});
+        .then(resp => setRockets(resp));
     },[rockets.length])
     return (
         <div style={{justifyContent: "space-between", display:"contents", overflowX:"hidden"}}>
-    <div  data-testId="list"  style = {{    
-        position: "fixed",
-        top: "108",
-        paddingBottom: "0",
-        height: "100%",
-        zIndex:1,
-        width: "220px",
-        overflowX: "scroll",
-        backgroundColor: "transparent"}}>
-        <ul style={{  
-            margin:"auto",  
-        width: "100%",
-        height: "inherit",
-        backgroundColor: "#E7E9EB",
-        overflowY:"scroll !important",
-        listStyleType:"none",
-        paddingTop: "20px"}}>
-   {rockets.map((data, index)=>(<li key={`${index}_item`} onClick={()=>dispatch(setSearchValue({page: "rockets", id: `${data.rocket_id}`}))} style={{ width:"140px" ,color:"darkblue",fontWeight:"bold" ,padding: "10px"}}><Link to="details">{data.rocket_name}</Link></li>))}
-        </ul>
-    </div>
+  <List page={"rockets"} list={rockets} linkTo={"rocket_name"} id={"rocket_id"}/>
     <div data-testId="rightpanel" className="rightPanel" >
 <h3 data-testId="overview">Rockets</h3>
 <p data-testId="content">In 2001, Musk conceptualised "Mars Oasis", a project to land a miniature experimental greenhouse containing seeds with dehydrated gel on Mars to grow plants on  Martian soil, "so this would be the furthest that life's ever travelled in an attempt to regain public interest in space exploration and increase the budget of NASA. But Musk realized that even with a much larger space budget,
@@ -53,15 +31,15 @@ It hired former NASA astronaut Ken Bowersox to oversee the department as a vice 
  However, it has since been reported that the former astronaut subsequently left SpaceX in late 2011.
   No reason was given and no replacement in that position has been announced.
   </p>
-<div style={{ paddingTop:"10px" ,textAlign:"left", width: "100%"}} >
+<div className="detailsListForRockets">
    {rockets.map(data => (
-    <div style={{ display:"initial", marginTop:"60px" ,textAlign:"left", width: "50px"}}>
-        <table style = {{marginTop:"20px"}}>
-         <tr><td style={{backgroundColor: "lightblue", width:"140px" ,fontWeight:"bold" ,padding: "10px"}}>Title</td><td style={{width:"1000px", paddingLeft:"30px", display:"flex", flexWrap:"word-wrap"}}>{data.rocket_name}</td></tr>
-         <tr><td style={{backgroundColor: "lightblue", width:"140px",fontWeight:"bold"  ,padding: "10px"}}>Details</td><td style={{width:"1000px", paddingLeft:"30px" , display:"flex", flexWrap:"word-wrap"}}>{data.description}</td></tr> 
-         <tr><td style={{backgroundColor: "lightblue", width:"140px" ,fontWeight:"bold" ,padding: "10px"}}>Country</td><td style={{width:"1000px" , paddingLeft:"30px", display:"flex", flexWrap:"word-wrap"}}>{data.country}</td></tr> 
-         <tr><td style={{backgroundColor: "lightblue", width:"140px" ,fontWeight:"bold" ,padding: "10px"}}>Success rate</td><td style={{width:"1000px" , paddingLeft:"30px", display:"flex", flexWrap:"word-wrap"}}>{data.success_rate_pct}</td></tr> 
-         <tr><td style={{backgroundColor: "lightblue", width:"140px" ,fontWeight:"bold" ,padding: "10px"}}>Article link</td><td style={{width:"1000px", paddingLeft:"30px" , display:"flex", flexWrap:"word-wrap"}}><a href={data.wikipedia}>{data.wikipedia}</a></td></tr> 
+    <div className="detailsListItem">
+        <table className="detailsListTable">
+         <tr><td className="detailsListTableRow">Title</td><td className="detailsTableDetails">{data.rocket_name}</td></tr>
+         <tr><td className="detailsListTableRow">Details</td><td className="detailsTableDetails">{data.description}</td></tr> 
+         <tr><td className="detailsListTableRow">Country</td><td className="detailsTableDetails">{data.country}</td></tr> 
+         <tr><td className="detailsListTableRow">Success rate</td><td className="detailsTableDetails">{data.success_rate_pct}</td></tr> 
+         <tr><td className="detailsListTableRow">Article link</td><td className="detailsTableDetails"><a href={data.wikipedia}>{data.wikipedia}</a></td></tr> 
          
          </table>
 
